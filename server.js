6384ecc732cdca6bf0bbe1341a724edb52896752
AndreyNik2 <105128068+AndreyNik2@users.noreply.github.com>
2023-02-26 13:55:43 +0200

    2nd

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 // CvKOhDmvkq3GvnbH
-const app = require('./app')
-const mongoose = require('mongoose');
+const app = require("./app");
+const mongoose = require("mongoose");
 
+const DB_HOST =
+  "mongodb+srv://Andrii:CvKOhDmvkq3GvnbH@cluster0.uh6nglu.mongodb.net/contactbook?retryWrites=true&w=majority";
 
-const DB_HOST = "mongodb+srv://Andrii:CvKOhDmvkq3GvnbH@cluster0.uh6nglu.mongodb.net/contactbook?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
 
-mongoose.set('strictQuery', true);
-
-mongoose.connect(DB_HOST)
-.then(app.listen(3000))
-.catch(error => console.log(error.message))
-
-
+mongoose
+  .connect(DB_HOST)
+  .then(app.listen(3000))
+  .catch((error) => console.log(error.message));
